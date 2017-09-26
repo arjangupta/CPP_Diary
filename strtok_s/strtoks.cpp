36ecfcac165c4c4d7f09c484db098619177838a8
Arjan Gupta <arjan.gupta@eyeverify.com>
2017-09-26 00:22:23 -0500

    Test the workings of strtok() and strtok_r()

--- strtok_s/strtoks.cpp
+++ strtok_s/strtoks.cpp
@@ -4,13 +4,38 @@
  */
 
 #include <string.h>
+#include <iostream>
 
+/* restrict does not natively exist in C++, but
+ * this program will be later translated to C.
+ */
 #define restrict __restrict__
 
-//char* strtok
+//precondition: str MUST be null-terminated
+/*char *strtok_sa(char *restrict str, size_t *restrict strmax,
+     const char *restrict delim, char **restrict ptr)
+{
+	if ()
+}*/
 
 int main () 
 {
+	char s1[] = "-abc-=-def";
+	char *sp, *x;
+	x = strtok_r(s1, "-", &sp);	// x = "abc", sp = "=-def"
+	x = strtok_r(NULL, "-=", &sp);	// x = "def", sp = NULL
+	//x = strtok_r(NULL, "=", &sp);	// x = NULL
+
+	printf("%s\n", x);
+
+	char s2[] = "-abc-=-defdef";
+	x = strtok(s2, "-");
+	x = strtok(NULL, "-=");
+
+	printf("%s\n", x);
+
+	printf("%lu\n", strlen(x));
+
 	return 0;
 } 
 
