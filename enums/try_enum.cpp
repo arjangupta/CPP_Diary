307f2caa2b1708d3f2b84fa20ebafa13a2789783
Arjan Gupta <arjangupta95@gmail.com>
2018-01-15 14:51:35 -0600

    Add a overload for << operator in enum printing

--- enums/try_enum.cpp
+++ enums/try_enum.cpp
@@ -1,12 +1,28 @@
-/* Purpose: Just trying out an enum class
+/* Author: Arjan Gupta
+ * Purpose: Just trying out an enum class
  */
 
 #include <iostream>
 
 enum class Color { red, green, blue = 100 };
 
+std::ostream& operator<<(std::ostream& os, Color c)
+{
+    switch (c)
+    {
+        case Color::red   : os << "red";    break;
+        case Color::green : os << "green";  break;
+        case Color::blue  : os << "blue";   break;
+        default    : os.setstate(std::ios_base::failbit);
+    }
+    return os;
+}
+
 int main ()
 {
 	Color which_color = Color::green;
-	std::cout << "Color is " << static_cast<int>( which_color ) << std::endl;
+	std::cout << "Color is " << which_color << std::endl;
+	std::cout << "Color value is " << static_cast<int>( which_color ) << std::endl;
+
+	return 0;
 }
