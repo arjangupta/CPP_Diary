b9172454c5bdd47e080f577a0a57c226c81ee56e
Gupta, Arjan <arjangupta95@gmail.com>
2018-01-21 06:46:29 -0600

    Write set up code for a reduce mesh program

---
+++ cpp-application/reduce-mesh/reduce_mesh.cpp
@@ -0,0 +1,11 @@
+/* Author: Arjan Gupta
+ * Purpose: Reduce a given mesh of points to one point
+ */
+
+#include <iostream>
+
+int main ()
+{
+	std::cout << "\nReduce a given mesh to one point!\n\n";
+	return 0;
+}
