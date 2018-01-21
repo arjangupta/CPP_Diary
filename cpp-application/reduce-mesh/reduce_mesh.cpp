f5a37d60c86fb06fd9fe79afa77de662db411fb3
Gupta, Arjan <arjangupta95@gmail.com>
2018-01-21 07:28:57 -0600

    Show user the mesh

--- cpp-application/reduce-mesh/reduce_mesh.cpp
+++ cpp-application/reduce-mesh/reduce_mesh.cpp
@@ -1,11 +1,52 @@
 /* Author: Arjan Gupta
- * Purpose: Reduce a given mesh of points to one point
+ * Purpose: Reduce a given mesh of points to one point. For the sake of
+ *          demonstration, we will multiply values at the points
  */
 
+// Std lib includes
 #include <iostream>
+#include <cstddef>
+
+// global consts (instead of macros)
+const size_t total_mesh_height = 3; 
+const size_t total_mesh_width = 3;
+
+void show_mesh ( int** total_mesh, size_t height, size_t width )
+{
+	for ( size_t i = 0; i < height; ++i )
+	{
+		for ( size_t j = 0; j < width; ++j )
+		{
+			std::cout << total_mesh[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}	
+}
+
+void reduce_mesh ( int** total_mesh, size_t height, size_t width )
+{
+	
+}
 
 int main ()
 {
 	std::cout << "\nReduce a given mesh to one point!\n\n";
+
+	int** total_mesh = new int* [total_mesh_height];
+
+	for ( size_t i = 0; i < total_mesh_height; ++i )
+	{
+		total_mesh[i] = new int [total_mesh_width]; 
+
+		for ( size_t j = 0; j < total_mesh_width; ++j )
+		{
+			total_mesh[i][j]  = 1 + i + j;
+		}
+	}
+
+	show_mesh( total_mesh, total_mesh_height, total_mesh_width );
+
+	std::cout << std::endl;
+
 	return 0;
 }
