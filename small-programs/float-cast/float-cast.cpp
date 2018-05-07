5f5f4eb172a5dae42d29190a11a7ff3d208896d6
Arjan Gupta <arjangupta95@gmail.com>
2018-05-07 14:13:42 -0500

    Do assignment instead of casts

--- small-programs/float-cast/float-cast.cpp
+++ small-programs/float-cast/float-cast.cpp
@@ -3,18 +3,19 @@
 int main()
 {
     uint8_t byte_a = 'a';
-    float float_of_a = static_cast<float>(byte_a);
+    float float_of_a = byte_a;
     float_of_a += 0.5;
-    std::cout << "Cast 'a' to a float and add 0.5: " << float_of_a << std::endl;
+    std::cout << "Assign 'a' to a float and add 0.5: " << float_of_a << "\n\n";
 
     uint8_t byte_arr[4];
     for (size_t i = 0; i < 4; ++i)
     {
         byte_arr[i] = 'a' + i;
     }
-    float float_of_arr = static_cast<float>(*(byte_arr+2));
+    float float_of_arr = *(byte_arr+2);
     float_of_arr += 0.5;
-    std::cout << "Cast byte_array[2] to a float and add 0.5: " << float_of_arr << std::endl;
+    std::cout << "Assign byte_array[2] to a float and add 0.5: " << float_of_arr << "\n\n";
+
 
     std::cout << "The size of a float is " << sizeof(float) << std::endl;
 
