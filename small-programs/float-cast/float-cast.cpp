a9c96d0c8869930a2026a01247d6681f55383387
Arjan Gupta <arjangupta95@gmail.com>
2018-05-07 13:52:20 -0500

    Float casting from char to float

--- small-programs/float-cast/float-cast.cpp
+++ small-programs/float-cast/float-cast.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
-    std::cout << "Do a float cast\n";
+    uint8_t byte_a = 'a';
+    float float_of_a = static_cast<float>(byte_a);
+    float_of_a += 0.5;
+    std::cout << float_of_a << std::endl; 
     return 0;
 }
