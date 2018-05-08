b323a68763aebc1fa4a5cf16d43cdd49e627df59
Arjan Gupta <arjangupta95@gmail.com>
2018-05-08 10:47:29 -0500

    Going from uint8 arr to uint32 to float to uint32

--- small-programs/float-cast/float-cast.cpp
+++ small-programs/float-cast/float-cast.cpp
@@ -25,21 +25,33 @@ int main()
     std::cout << "Cast 99.5 to uint16_t: " << static_cast<uint16_t>(float_of_elem) << "\n";
     std::cout << "Cast 99.5 to uint32_t: " << static_cast<uint32_t>(float_of_elem) << "\n\n";
 
-    //uint8_mx
+    //uint8_mx, wrapping
     std::cout << "The max uint8_t is: " << UINT8_MAX << "\n";
     uint8_t uint8max_plus1_plusa = UINT8_MAX + 1 + 97;
     std::cout << "The max uint8_t + 1 + 97 is: " << uint8max_plus1_plusa << "\n\n";
 
-    //uint32_max
+    //uint32_max, assignment to float
     uint32_t uint32_max = UINT32_MAX;
     std::cout << "The max uint32_t is: " << uint32_max << "\n";
     float uint32max_float = UINT32_MAX;
     std::cout << std::setiosflags(std::ios::fixed) << "Assign max of uint32_t to float: " << uint32max_float << "\n\n";
 
     //memcpy from uint8 to uint32
-    uint32_t uint8arr_to_uint32 = 0;
-    memcpy(&uint8arr_to_uint32, byte_arr, 4);
-    std::cout << "memcpy a byte array (4-long) to uint32: " << uint8arr_to_uint32 << "\n\n";
+    uint32_t byte_array_to_uint32 = 0;
+    memcpy(&byte_array_to_uint32, &byte_arr[0], 4);
+    std::cout << "memcpy a byte array (4-long) to uint32: " << byte_array_to_uint32 << "\n";
+    
+    //uint32 to float
+    float byte_array_float = byte_array_to_uint32;
+    std::cout << "Assignment of a uint32 to a float: " << byte_array_float << "\n";
+    
+    //float to uint32
+    uint32_t convert_from_float = byte_array_float;
+    std::cout << "Assignment of float to uint32: " << convert_from_float << "\n";
+    
+    //float to byte-array
+    *byte_arr = static_cast<uint32_t>(byte_array_float);
+    std::cout << "Cast float to uint32_t and assign to 4-long byte array: " << static_cast<int>(byte_arr[0]) << byte_arr[1] << byte_arr[2] << byte_arr[3] << "\n\n";
 
     //size of float
     std::cout << "The size of a float is " << sizeof(uint32max_float) << std::endl;
