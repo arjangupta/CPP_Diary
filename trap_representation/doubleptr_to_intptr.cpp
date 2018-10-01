48efba66dac80a7257c3f2cee3dfeb52a9c9da49
arjangupta <arjangupta95@gmail.com>
2018-10-01 09:05:27 -0500

    Add comment to show expected output

--- trap_representation/doubleptr_to_intptr.cpp
+++ trap_representation/doubleptr_to_intptr.cpp
@@ -3,14 +3,14 @@
 
 int main()
 {
-	double d = 3.14159;
+    double d = 3.14159;
     uint8_t * p = (uint8_t*) &d;
     *p = 0;
     
     for (int i = 0; i < sizeof(double); i++)
     {
         std::cout << std::hex << (int)p[i] << "\t";
-    }
+    }//output: 0       86      1b      f0      f9      21      9       40
 
     std::cout << std::endl;
 
