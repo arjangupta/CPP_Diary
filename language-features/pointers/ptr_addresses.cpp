d19e6e3bd0c29dc4407933cecd88ea21536f35aa
Arjan Gupta <arjangupta95@gmail.com>
2018-10-04 19:46:07 -0500

    Show output in comments

--- language-features/pointers/ptr_addresses.cpp
+++ language-features/pointers/ptr_addresses.cpp
@@ -24,4 +24,15 @@ int main()
               << "Are p and q pointing to addresses at which there are ints of the same value? " << yesOrNo(*p == *q) << std::endl
               << "Are p and r pointing to addresses at which there are ints of the same value? " << yesOrNo(*p == *r) << std::endl
               << std::endl;
+
+    // OUTPUT (ptr values will vary):
+    //
+    // Are p and q pointing to the same address? Yes.
+    // Are p and r pointing to the same address? No.
+    // Value of p: 0x7ffeeb9d173c
+    // Value of q: 0x7ffeeb9d173c
+    // Value of r: 0x7ffeeb9d1724
+    //
+    // Are p and q pointing to addresses at which there are ints of the same value? Yes.
+    // Are p and r pointing to addresses at which there are ints of the same value? Yes.
 }
