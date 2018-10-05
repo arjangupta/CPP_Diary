91e5a85b7cc8893cfea83e1d9be07159651a9209
Arjan Gupta <arjangupta95@gmail.com>
2018-10-04 19:33:18 -0500

    Clean up formatting for ptr_addresses.cpp

--- language-features/pointers/ptr_addresses.cpp
+++ language-features/pointers/ptr_addresses.cpp
@@ -8,17 +8,16 @@ std::string yesOrNo(bool expression)
 
 int main()
 {
-    int i = 9;
+    int i  = 9;
     int *p = &i;
     int *q = p;
-    int j = 9;
+    int j  = 9;
     int *r = &j;
 
-    std::cout << "Are p and q pointing at the same address? " << yesOrNo(p == q) << std::endl;
-    
-    std::cout << "Are p and r pointing at the same address? " << yesOrNo(p == r) << std::endl;
-    
+    std::endl;
+    std::cout << "Are p and q pointing at the same address? "         << yesOrNo(p == q)   << std::endl;
+    std::cout << "Are p and r pointing at the same address? "         << yesOrNo(p == r)   << std::endl;
     std::cout << "Are p and q pointing to an int of the same value? " << yesOrNo(*p == *q) << std::endl;
-    
     std::cout << "Are p and r pointing to an int of the same value? " << yesOrNo(*p == *r) << std::endl;
+    std::endl;
 }
