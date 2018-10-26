5f658a1f3a333c11d9fb1230133f1804578b5fb1
Arjan Gupta <arjangupta95@gmail.com>
2018-10-26 13:57:21 -0500

    Add simple string parser prog

---
+++ language-features/string-parse/string_parse_comma.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+int main()
+{
+    //Declarations
+    std::string str = "4";//,5,6,7";
+    std::vector<int> int_vect;
+    std::stringstream ss(str);
+
+    //Iterate over string stream
+    int i = 0;
+    int count = 0;
+    while (ss >> i)
+    {
+        int_vect.push_back(i);
+        if (ss.peek() == ',')
+        {
+            ss.ignore();
+        }
+        ++count;    
+    }
+
+    int first = 0;
+    int second = 0;
+    bool is_pair = false; 
+
+    //Only need first two vals
+    if (count >= 2)
+    {
+        is_pair = true;
+        first  = int_vect[0];
+        second = int_vect[1];
+    }
+    
+    //Display vals if they were a pair
+    if (is_pair)
+    {
+        std::cout << "First val is: " << first << std::endl;
+        std::cout << "Second val is: " << second << std::endl;  
+    }
+    else
+    {
+        std::cout << "String didn't have a pair." << std::endl;
+    }
+
+    return 0;
+}
