f2c4d71b94e96ad9bbc217a8cf053d0a3e42aeb6
Arjan Gupta <arjangupta95@gmail.com>
2019-08-11 23:27:47 -0500

    Begin writing asyncDoubleElems function signature

--- language-features/unique-ptrs-threads/unique_ptrs_thread.cpp
+++ language-features/unique-ptrs-threads/unique_ptrs_thread.cpp
@@ -1,9 +1,18 @@
+#include <memory>
+#include <string>
 #include <iostream>
 
-//TODO: Create a sample class holding some memory
+//Wait for half a second and 
+//double each element
+void asyncDoubleElems(std::unique_ptr<std::string> thread_name_ptr,
+                      std::unique_ptr< std::vector<uint32_t> > input_vector_ptr )
+{
+
+}
 
 int main()
 {
     std::cout << "Unique ptrs + threads" << std::endl;
     return 0;
 }
+
