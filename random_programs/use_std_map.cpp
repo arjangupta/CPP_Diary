4e52eb56550030a16b245654a7292c6433ef910f
Arjan Gupta <arjangupta95@gmail.com>
2019-08-13 00:16:47 -0500

    Use an unordered map instead

--- random_programs/use_std_map.cpp
+++ random_programs/use_std_map.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <map>
+#include <unordered_map>
+#include <string>
 
 int main()
 {
-    std::map<std::string, unsigned long> people_net_worth;
-    people_net_worth["Bill Gates"] = 103700000000;
-    people_net_worth["Jeff Bezos"] = 112500000000;
-    people_net_worth["Elon Musk"]  = 19900000000;
+    std::unordered_map<std::string, std::string> people_net_worth;
+    people_net_worth["Bill Gates"] = "103700000000";
+    people_net_worth["Jeff Bezos"] = "112500000000";
+    people_net_worth["Elon Musk"]  = "19900000000";
+    people_net_worth["John Doe"]   = "55000";
 
     for (auto const& big_guy : people_net_worth)
     {
