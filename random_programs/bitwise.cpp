1ce9f6e4702076ceb5b74f8e7e802a5a6865432d
Arjan Gupta <agupta@intellifarms.com>
2020-02-21 09:57:49 -0600

    Testing left and right shifting to keep values between 1 and 0

--- random_programs/bitwise.cpp
+++ random_programs/bitwise.cpp
@@ -11,5 +11,15 @@ int main()
     std::cout << "The bitwise AND of " << 4
               << " and " << resultant_number << " is "
               << (4 & resultant_number) << std::endl;
+
+    
+    uint8_t byte = 127;
+    uint16_t* data = reinterpret_cast<uint16_t*>(&byte);
+    std::cout << "The data uint16_t pointer points to " << *data << std::endl;
+    std::cout << "The data casted to uint8_t* and deref'd is " << (int)*((uint8_t*) data) << std::endl;
+    // & out the 8th bit
+    uint16_t right_shift_result = (*((uint8_t*) data) & (1 << 8)) >> 8;
+    std::cout << "The right shift result is " << right_shift_result << std::endl;
+
     return 0; 
 }
