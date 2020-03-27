d1451c0cbf3701a74244e5fb83d1c6326615c043
Arjan Gupta <arjangupta95@gmail.com>
2020-03-27 18:46:29 -0500

    Mass allocate 100 MB chunks

--- small_programs/memory_monitor/memory_monitor.cpp
+++ small_programs/memory_monitor/memory_monitor.cpp
@@ -23,13 +23,28 @@ int main()
     << "Size of std::string is " << sizeof(std::string) << " bytes." << std::endl
     << "Size of ArbitraryData is " << sizeof(ArbitraryData) << " bytes." << std::endl << std::endl;
 
+    // Alloc pointers to hold 1 GB
+    // 40 * 25 = 1 KB, 1 KB * 1000 = 1 MB, 1 MB * 1000 = 1 GB
+    ArbitraryData** data_array = (ArbitraryData**) std::malloc(sizeof(ArbitraryData*) * 25 * 1000 * 1000);
+
     // Check the amount of available memory
     std::cout << "Checking the amount of free memory before any allocs" << std::endl;
     system("free --mega");
 
-    // TODO: Write loop for mass allocs
+    std::cout << " --------- Starting the mass allocs in 100 MB chunks ---------- " << std::endl;
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << "Batch number: " << i << std::endl;
+        int one_gb_factor = (25 * 1000 * 100);
+        for (int j = 0; j < (25 * 1000 * 100); ++j)
+        {
+            data_array[(i * one_gb_factor) + j] = new ArbitraryData(3, 23.5, "polar_bear");
+        }
+    }
 
-    //ArbitraryData* data_point1 = new ArbitraryData(3, 23.5, "polar_bear");
+    // Check the amount of available memory
+    std::cout << "Checking the amount of free memory after all allocs" << std::endl;
+    system("free --mega");
 
     std::cout << "Ending program...." << std::endl;
 
