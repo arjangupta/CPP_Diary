f826fb2cc64afa0e48738c7a3c18ba039460498d
Arjan Gupta <arjangupta95@gmail.com>
2020-03-27 19:33:47 -0500

    Move the mass allocations to massAlloc()

--- small_programs/memory_monitor/memory_monitor.cpp
+++ small_programs/memory_monitor/memory_monitor.cpp
@@ -15,21 +15,11 @@ struct ArbitraryData
     std::string name;
 };
 
-int main()
+void massAlloc()
 {
-    std::cout << "Some type information -" << std::endl
-    << "Size of int is " << sizeof(int) << " bytes." << std::endl
-    << "Size of float is " << sizeof(float) << " bytes." << std::endl
-    << "Size of std::string is " << sizeof(std::string) << " bytes." << std::endl
-    << "Size of ArbitraryData is " << sizeof(ArbitraryData) << " bytes." << std::endl << std::endl;
-
     // Alloc pointers to hold 1 GB
     // 40 * 25 = 1 KB, 1 KB * 1000 = 1 MB, 1 MB * 1000 = 1 GB
     ArbitraryData** data_array = (ArbitraryData**) std::malloc(sizeof(ArbitraryData*) * 25 * 1000 * 1000);
-
-    // Check the amount of available memory
-    std::cout << "Checking the amount of free memory before any allocs" << std::endl;
-    system("free --mega");
 
     std::cout << " --------- Starting the mass allocs in 100 MB chunks ---------- " << std::endl;
     for (int i = 0; i < 10; ++i)
@@ -41,6 +31,21 @@ int main()
             data_array[(i * one_gb_factor) + j] = new ArbitraryData(3, 23.5, "polar_bear");
         }
     }
+}
+
+int main()
+{
+    std::cout << "Some type information -" << std::endl
+    << "Size of int is " << sizeof(int) << " bytes." << std::endl
+    << "Size of float is " << sizeof(float) << " bytes." << std::endl
+    << "Size of std::string is " << sizeof(std::string) << " bytes." << std::endl
+    << "Size of ArbitraryData is " << sizeof(ArbitraryData) << " bytes." << std::endl << std::endl;
+
+    // Check the amount of available memory
+    std::cout << "Checking the amount of free memory before any allocs" << std::endl;
+    system("free --mega");
+
+    massAlloc();
 
     // Check the amount of available memory
     std::cout << "Checking the amount of free memory after all allocs" << std::endl;
