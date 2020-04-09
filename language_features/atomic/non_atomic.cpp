4fe3b427406f53f4bcf2ba223dfd6ce105de2ce3
Arjan Gupta <arjangupta95@gmail.com>
2020-04-08 22:33:05 -0500

    Give ids to the threaded functions

--- language_features/atomic/non_atomic.cpp
+++ language_features/atomic/non_atomic.cpp
@@ -1,6 +1,3 @@
-// Program adapted from ModernesCpp.com
-// https://www.modernescpp.com/index.php/the-atomic-boolean
-
 #include <vector>
 #include <thread>
 #include <condition_variable>
@@ -12,20 +9,21 @@ std::condition_variable _cond_var;
 
 bool _data_ready;
 
-void waitingForWork()
+void waitingForWork(int id)
 {
-    std::cout << "Waiting..." << std::endl;
+    std::cout << "T" << id << ": Waiting..." << std::endl;
     
     std::unique_lock<std::mutex> work_lock(_main_mutex);
     _cond_var.wait(work_lock, []{return _data_ready;});
 
     _shared_work[1] = 2;
 
-    std::cout << "Work done." << std::endl;
+    std::cout << "T" << id << ": Work done." << std::endl;
 }
 
-void setDataReady()
+void setDataReady(int id)
 {
+    std::cout << "T" << id << ": Create the vector." << std::endl;
     _shared_work = {1, 0, 3};
 
     {
@@ -33,19 +31,22 @@ void setDataReady()
         _data_ready = true;
     }
 
-    std::cout << "Data prepared, ready to notify." << std::endl;
-    _cond_var.notify_one();
+    std::cout << "T" << id << ": Data prepared, notifying..." << std::endl;
+    _cond_var.notify_all();
 }
 
 int main()
 {
     std::cout << std::endl;
+    _data_ready = false;
 
-    std::thread t1(waitingForWork);
-    std::thread t2(setDataReady);
+    std::thread t1(waitingForWork, 1);
+    std::thread t2(waitingForWork, 2);
+    std::thread t3(setDataReady, 3);
 
     t1.join();
     t2.join();
+    t3.join();
 
     std::cout << "The vector elements are as follows." << std::endl;
     for ( auto v : _shared_work )
