dda3d5c24fd11589692112d735a9d9e320b6f3c6
Arjan Gupta <agupta@intellifarms.com>
2021-01-14 06:35:10 -0600

    Print the 78th position of the file

--- small_programs/get_ram/get_ram.cpp
+++ small_programs/get_ram/get_ram.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 int main()
 {
     // Execute the command to get the memory
     system("free -m | grep Mem: > mem.txt");
+
+    // Read the output file
+    std::ifstream filstream("./mem.txt");
+    std::string file_contents((std::istreambuf_iterator<char>(filstream)), std::istreambuf_iterator<char>());
+
+    // Look for the first number at the end of the line
+    std::cout << "At pos 79, the file reads: " << file_contents[78] << std::endl;
+
+    // Print out available memory
+
     return 0;
 }
