28a811955ed60030047047f3c3f2a943af3e9b13
Arjan Gupta <agupta@intellifarms.com>
2021-01-14 07:03:35 -0600

    Get available memory

--- small_programs/get_ram/get_ram.cpp
+++ small_programs/get_ram/get_ram.cpp
@@ -11,13 +11,9 @@ int main()
     std::ifstream filstream("./mem.txt");
     std::string file_contents((std::istreambuf_iterator<char>(filstream)), std::istreambuf_iterator<char>());
 
-    // Look for the first number at the end of the line
-    std::cout << "At pos 79, the file reads: " << file_contents[78] << std::endl;
-
     // From the end of the file, rfind the first space
     std::size_t last_space = file_contents.rfind(" ", (file_contents.size() - 1));
-    std::cout << "Last space to the end of file: " << file_contents.substr(last_space + 1) << std::endl;
-    std::cout << "And, converted to int: " << std::stoi(file_contents.substr(last_space + 1)) << std::endl;
+    std::cout << "Last space to end of file, converted to int (available RAM): " << std::stoi(file_contents.substr(last_space + 1)) << std::endl;
 
     return 0;
 }
