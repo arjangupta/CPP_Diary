a8f8eed716d70cb9e2f1d3212a749154b2c4339e
Arjan Gupta <agupta@intellifarms.com>
2021-01-14 07:17:30 -0600

    Print out available and total memory

--- small_programs/get_ram/get_ram.cpp
+++ small_programs/get_ram/get_ram.cpp
@@ -13,13 +13,17 @@ int main()
 
     // From the end of the file, rfind the first space
     std::size_t last_space = file_contents.rfind(" ", (file_contents.size() - 1));
-    std::cout << "Last space to end of file, converted to int (available RAM): " << std::stoi(file_contents.substr(last_space + 1)) << std::endl;
+    int available_ram = std::stoi(file_contents.substr(last_space + 1));
+    std::cout << "Last space to end of file, converted to int (available RAM): " << available_ram << std::endl;
 
     // From the beginning of the file, find the first colon
     std::size_t first_colon = file_contents.find(":");
     // From here, find first non-space
-    std::size_t first_number = file_contents.find_first_not_of(" ");
-    std::cout << "First not space after colon is: " << file_contents[first_number] << std::endl;
+    std::size_t first_number = file_contents.find_first_not_of(" ", first_colon + 1);
+    // Get full number
+    std::size_t end_of_first_num = file_contents.find_first_of(" ", first_number);
+    int total_ram = std::stoi(file_contents.substr(first_number, end_of_first_num));
+    std::cout << "Total memory in int: " << total_ram << std::endl;
 
     return 0;
 }
