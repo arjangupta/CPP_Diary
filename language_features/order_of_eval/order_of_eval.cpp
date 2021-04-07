096f7c779de96558e580284d2e47579a3a67c974
Arjan Gupta <agupta@intellifarms.com>
2021-04-07 18:31:35 -0500

    Add first logic chain experiment

--- language_features/order_of_eval/order_of_eval.cpp
+++ language_features/order_of_eval/order_of_eval.cpp
@@ -6,7 +6,44 @@ bool funcA(bool val)
     return val;
 }
 
+bool funcB(bool val)
+{
+    std::cout << "Function B!" << std::endl;
+    return val;
+}
+
+bool funcC(bool val)
+{
+    std::cout << "Function C!" << std::endl;
+    return val;
+}
+
+bool funcD(bool val)
+{
+    std::cout << "Function D!" << std::endl;
+    return val;
+}
+
+bool funcE(bool val)
+{
+    std::cout << "Function E!" << std::endl;
+    return val;
+}
+
 int main()
 {
+    // Chain up conditionals
+    if ( funcA(false) 
+        || funcB(false)
+        || (funcC(false) && funcD(true))
+        || funcE(true) )
+    {
+        std::cout << "Eval 1 TRUE!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Eval 1 FALSE!" << std::endl;
+    }
+
     return 0;
 }
